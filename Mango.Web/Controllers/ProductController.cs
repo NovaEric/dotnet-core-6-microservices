2ae26d89f6e00e372bf8dada993ef06c89d6d169
Eric Nova <Ericnova23@hotmail.com>
2022-08-19 16:19:25 -0500

    Section4-40

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductRequestDto> list = new();
+            var response = await _productService.GetAllProductsAsync<ProductResponseDto>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductRequestDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
